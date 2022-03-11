66486a207ff7848fdc1e365ac94155f7821c6096
BhadsaleA <aakankshabhadsale8961@gmail.com>
2022-03-11 23:15:07 +0530

    My first commit

---
+++ basic.py
@@ -0,0 +1,32 @@
+#print("aakanksha is preparing well")
+
+""""
+String Functions
+1)count()
+"""
+"""""
+string_a="Pankaj is very good teacher Pankaj"
+sub_string='Pankaj'
+pankaj_count=string_a.count(sub_string)
+print(pankaj_count)
+"""
+
+""""
+2)replace() -> replacing the string
+syntax :- string.replace(string want to replace,replace string)
+"""
+#string_b = "Pankaj is very good"
+#string_b = string_b.replace('Pankaj','Aakanksha')
+#print(string_b)
+
+""""
+3) split() -> split the string
+It convert the string into list using blankspace(whitspaces)
+"""
+#string_c = "Aakanksha Shraddha Pankaj Soham "
+#print(string_c)
+#l = string_c.split()
+#print(l)
+
+#sp = string_c.split('k')
+#print(sp)
